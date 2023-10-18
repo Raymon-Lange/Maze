497dcefc63337bac350765eb40c856f2759691ae
Raymon.Lange <raymon.lange@gmail.com>
2023-10-18 12:35:26 -0500

    Tremauxs working

--- TremauxsSolve.py
+++ TremauxsSolve.py
@@ -12,54 +12,56 @@ class TremauxsSolve:
         #path is the short path to solve the maze
         self.path = []
 
-        def solve(self, start, end):
-            self.start = start
-            self.end = end
+        # this is a list of all junctions, a junction is more than one open space
+        self.junctions = []
 
-            found = False 
+        self.directions= [(0,1),(0,-1),(1,0),(-1,0)]
 
-            #Add the start
-            self.wasHere.append(self.start)
+    #Check to see if the point we moved to has more than option 
+    #return True if you can move two or more option otherwise false
+    def isJunction(self, x, y):
 
-            while not found:
+        openSpace  = 0
 
-                pass
-
-
-        #Check to see if the point we moved to has more than option 
-        #return True if you can move two or more option otherwise false
-        def isJunction(self, x, y):
-
-            if maze[x][y] != 1: 
-                pass
-            if maze[x][y] != 1: 
-                pass
-            if maze[x][y] != 1: 
-                pass
-            if maze[x][y] != 1: 
-                pass
+        if x != 0 :
+            if self.maze[x-1][y] != 1: 
+                openSpace +=1 
             
+        if x != self.height-1 :
+            if self.maze[x-1][y] != 1: 
+                openSpace +=1
+            
+        if y != self.width-1 :
+            if self.maze[x][y+1] != 1: 
+                openSpace +=1
 
-            return False 
+        if y != 0 :
+            if self.maze[x][y-1] != 1: 
+                openSpace +=1
+
+        if openSpace >= 2:
+            return True
+            
+        return False 
         
 
         #If all direction are dead return true
-        def isDedend(self,x,y):
+    def isDedend(self,x,y):
             totalWall = 0
             if x != 0 :
-                if maze[x-1][y] == 1: 
+                if self.maze[x-1][y] == 1: 
                     totalWall +=1 
             
             if x != self.height-1 :
-                if maze[x-1][y] != 1: 
+                if self.maze[x-1][y] == 1: 
                     totalWall +=1 
             
             if y != self.width-1 :
-                if maze[x][y+1] != 1: 
+                if self.maze[x][y+1] == 1: 
                     totalWall +=1 
 
             if y != 0 :
-                if maze[x][y-1] != 1: 
+                if self.maze[x][y-1] == 1: 
                     totalWall +=1 
 
             #if we can't move any direction, the point behind us should be open 
@@ -68,4 +70,79 @@ class TremauxsSolve:
                 return True
             
             return False
+    
+    def goBackToLastJuction(self):
+        lastJunction = self.junctions[len(self.junctions)]
+
+
+        isJuction = False
+
+        while not isJuction:
+
+            backStep = self.path.pop() 
+
+            if lastJunction == backStep:
+                isJuction = True
+            else:
+                self.maze[backStep[0]][backStep[1]] = 1
+
+                
+        
+    def solve(self, start, end):
+            self.start = start
+            self.end = end
+
+            found = False 
+
+            #Add the start
+            self.wasHere.append(self.start)
+            self.path.append(self.start)
+
+            count = 0 
+
+            while not found:
+                direction = self.directions[random.randint(0,3)]
+                currentPos = self.wasHere[len(self.wasHere)-1]
+                x = currentPos[0] + direction[0]
+                y = currentPos[1] + direction[1]
+
+                if end[0] == x and end[1] == y:
+                    found = True
+                    self.path.append((x,y))
+                    return True
+
+
+                if self.maze[x][y] != 1 and (x,y) in self.wasHere:
+                    
+                    if self.isJunction(x,y):
+ 
+                        if (x,y) in self.junctions:
+                        #Get the last  postion i came from and mark in as wall 
+                            point = self.wasHere.pop()
+                            self.maze[point[0]][point[1]] = 1
+                        
+                            self.goBackToLastJuction()
+                        else: 
+                            #add the junction to the list of junctions 
+                            self.junctions.append((x,y))
+
+                    elif self.isDeadEnd():
+                        self.goBackToLastJuction()
+
+                    else:
+                        self.wasHere.append((x,y))
+                        self.path.append((x,y))
+
+                count += 1
+                if count > 100:
+                    return False
+
+                    
+
+
+
+                    
+
+
+                
             
